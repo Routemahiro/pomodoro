06041140aa20a87ffbff78ed0345faee6725c0ef
Route1212 <mint.jam0219@gmail.com>
2023-07-28 06:03:03 +0900

    メインウィンドウいじり中

--- view/main_window.py
+++ view/main_window.py
@@ -1,34 +1,57 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QLabel, QWidget, QLCDNumber
-from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout,QApplication
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
+
 import sys
 
-class Mainwindow(QMainWindow):
+class Mainwindow(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.setGeometry(300,300, 400, 200)
         self.setWindowTitle("Pomodoro Timer")
-        self.setGeometry(400, 180, 570, 405)
 
-        # Create a timer
-        self.timer = QLCDNumber(self)
-        self.timer.setDigitCount(5)  # 00:00
+        self.timer_label = QLabel("00:00", self)
+        self.timer_label.setAlignment(Qt.AlignCenter)
+        self.timer_label.setFont(QFont('Arial', 50))
 
-        # Create buttons
         self.start_button = QPushButton("Start", self)
+
         self.data_dl_button = QPushButton("", self)
+        self.data_dl_button.setStyleSheet(
+            "QPushButton {"
+            "background-color: #ff6347;"
+            "border-style: outset;"
+            "border-width: 2px;"
+            "border-radius: 50px;"
+            "border-color: beige;"
+            "}"
+        )
+        self.data_dl_button.setFixedSize(100, 100)
+
         self.setting_button = QPushButton("", self)
+        self.setting_button.setStyleSheet(
+            "QPushButton {"
+            "background-color: #ff6347;"
+            "border-style: outset;"
+            "border-width: 2px;"
+            "border-radius: 50px;"
+            "border-color: beige;"
+            "}"
+        )
+        self.setting_button.setFixedSize(100, 100)
 
-        # Create layout
         layout = QVBoxLayout()
-        layout.addWidget(self.timer)
+        layout.addWidget(self.timer_label)
         layout.addWidget(self.start_button)
-        layout.addWidget(self.data_dl_button)
-        layout.addWidget(self.setting_button)
 
-        # Set layout
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        button_layout = QHBoxLayout()
+        button_layout.addStretch(1)
+        button_layout.addWidget(self.data_dl_button)
+        button_layout.addWidget(self.setting_button)
+        
+        layout.addLayout(button_layout)
+
+        self.setLayout(layout)
 
 def main():
     app = QApplication(sys.argv)
