106b1d08c076be61110b187b8fe73384258814d9
ChisatoProgram <routemahiro@gmail.com>
2024-01-12 16:50:18 +0900

    直前のremaining_timeの統一化は早々にやめた
    メモ、ここのstop~startの意味がよくわからない。
    
    と書いておいたところから内容をまた見ていく

--- controller/timer_controller.py
+++ controller/timer_controller.py
@@ -58,26 +58,6 @@ class Timer:
         except Exception as e:
             print(f"An exception occurred in Timer's run method: {e}")
 
-import threading
-
-class RemainingTimeManager:
-    def __init__(self, initial_time):
-        self._remaining_time = initial_time
-        self._lock = threading.Lock()
-
-    def get_remaining_time(self):
-        with self._lock:
-            return self._remaining_time
-
-    def set_remaining_time(self, new_time):
-        with self._lock:
-            self._remaining_time = new_time
-
-    def decrement_remaining_time(self):
-        with self._lock:
-            self._remaining_time -= 1
-
-
 class PomodoroTimer:
     def __init__(self, timer_controller, session_id, work_time, break_time, work_callback, break_callback,update_ui_callback):
         print("PomodoroTimer is initialized.")  # Debug
@@ -93,7 +73,7 @@ class PomodoroTimer:
         self.activity_timer = Timer(60, self.update_work_activity)  # 1分ごとにupdate_work_activityを呼び出すタイマー
         self.update_ui_callback = update_ui_callback  # UIを更新するためのコールバック
         self.timer_paused = False
-        # ★ここともう一箇所で同一のコードで宣言されている。場合によっては統一してしまうことが必要かも
+        # ★ここともう一箇所で同一のコードで宣言されている。場合によっては統一してしまうことが必要かも？
         self.remaining_time = self.work_time
         self.last_ai_comment = None  # Add this line to initialize ai_comment
         self.pomodoro_id = 1  # ポモドーロIDを初期化
@@ -312,10 +292,10 @@ class TimerController:
         )
 
     def start_timer(self):
-        print("TimerController's start_timer is called")  # Debug
         self.start_session()  # Start a new session in the database
         self.pomodoro_timer.stop()
         self.pomodoro_timer.start()
+        # メモ、ここのstop~startの意味がよくわからない。
 
         self.cancel_timer = False  # タイマーのキャンセルフラグをリセット
 
